3fffc218c24bc9cdb99df42ff1fc68ef18187728
SaidNivek <kevin.vi.dias@gmail.com>
2022-05-17 12:25:15 -0400

    Footer complete, links to LinkedIn/GitHub, and CSS working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
-            Test Footer
+        <footer className="container text-center">
+            <h4><a href="https://www.linkedin.com/in/saidnivek/">LinkedIn</a> | <a href="https://github.com/SaidNivek">GitHub</a></h4>
         </footer>
     );
 }
