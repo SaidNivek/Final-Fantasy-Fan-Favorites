f0c8425a71baaa4326711d55d792412c451241f1
SaidNivek <kevin.vi.dias@gmail.com>
2022-05-17 21:28:16 -0400

    Fix footer colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="container text-center">
-            <h4><a href="https://www.linkedin.com/in/saidnivek/">LinkedIn</a> | <a href="https://github.com/SaidNivek">GitHub</a></h4>
+            <div className="link-div">
+                <a className="outside-link" href="https://www.linkedin.com/in/saidnivek/"><h4>LinkedIn</h4></a> 
+                <h4> | </h4>
+                <a href="https://github.com/SaidNivek"><h4>GitHub</h4></a>
+            </div>
+            {/* <h4>LinkedIn</a> | <a className="outside-link" href="https://github.com/SaidNivek">GitHub</a></h4>
+            <a href="#"><h4>Test</h4></a> */}
         </footer>
     );
 }
