bf586b0fbfadb5b53f6939c74244e19a59b245dc
SaidNivek <kevin.vi.dias@gmail.com>
2022-05-18 20:36:33 -0400

    Login/Register pages running, UI ready

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,11 +8,19 @@ function Register() {
         password2: '',
         email: ''
     })
-
+    // Deconstructs the formData object to not require us to use formData.username, formData.email, etc
+    // Now we can just call the username and have it point to formData.username, for easier/simpler syntax
     const {username, password, password2, email} = formData
 
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }))
+    }
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
     }
 
   return (
@@ -22,12 +30,13 @@ function Register() {
             <p>Please create an account</p>
         </div>
         <div className="container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label className="label" for="name">Username</label>
+                    <label className="label" for="username">Username</label>
                     <input
                         type='text'
                         value={username}
+                        id='username'
                         name='username'
                         placeholder='Please enter a username'
                         onChange={handleChange}
@@ -35,10 +44,11 @@ function Register() {
                     />
                 </div>
                 <div className="form-group">
-                    <label className="label" for="name">Password</label>
+                    <label className="label" for="password">Password</label>
                     <input
                         type='text'
                         value={password}
+                        id='password'
                         name='password'
                         placeholder='Please enter a password'
                         onChange={handleChange}
@@ -46,10 +56,11 @@ function Register() {
                     />
                 </div>
                 <div className="form-group">
-                    <label className="label" for="name">Confirm Password</label>
+                    <label className="label" for="password2">Confirm Password</label>
                     <input
                         type='text'
                         value={password2}
+                        id='password2'
                         name='password2'
                         placeholder='Please confirm password'
                         onChange={handleChange}
@@ -57,15 +68,19 @@ function Register() {
                     />
                 </div>
                 <div className="form-group">
-                    <label className="label" for="name">Email</label>
+                    <label className="label" for="email">Email</label>
                     <input
                         type='text'
                         value={email}
-                        name='username'
+                        id='email'
+                        name='email'
                         placeholder='Please enter your email'
                         onChange={handleChange}
                         className="form-control"  
                     />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-block btn-info">Submit</button>
                 </div>
             </form>
         </div>
