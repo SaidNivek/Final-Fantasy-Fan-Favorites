f269a2f715c60adf858961cea43b69c9ade0c9a5
SaidNivek <kevin.vi.dias@gmail.com>
2022-06-21 12:05:00 -0400

    Add about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,10 @@ const About = props => {
     return (
         <>
         <div>
-            <p>Test</p>
+            <h1>About Me</h1>
+            <p>I have always loved video games. Growing up, some of my earliest memories are of my oldest brother playing Final Fantasy while I watched, desperately wanting to play. In my need, I started my own game on the NES and saved over his hours of work on the original Final Fantasy. Later, when he bought a SNES, I watched him play FFVI for hours as he listened to music that still brings me back to that time - Alanis Morissette, Guns N' Roses, Metallica, Nirvana, and No Doubt.</p>
+            <p>This love of gaming has stayed with me throughout my life. I have completed almost all of the mainline Final Fantasy games, multiple spin-offs, and still would pick JRPGs as my favorite genre of game. The stories, the charatcers, the locations, and the music! The rich tapestry that is crafted so lovingly brings such joy to my heart and warmth to my soul.</p>
+            <p>Come and choose your favorites and let's see how your opinions fare against the masses. Is your favorite the best? Come, tell me why!</p>
         </div>
         <hr></hr>
         <Footer />
